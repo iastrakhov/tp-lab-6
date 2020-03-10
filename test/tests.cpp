4995813a2f8efbb38faec7d8b2472fb09c167f59
ilya Strakhov <strakhoff.k@mail.ru>
2020-03-11 00:55:54 +0300

    five try

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,9 +8,9 @@ TEST(Driver, calculate_payment)
 	WorkProject* project3 = new WorkProject("project_3", 300000);
 	vector<WorkProject*> projects = { project1, project2, project3 };
 	EmployeeFactory EF = EmployeeFactory(projects);
-	vector<string> data = { "Watts Richard Mark", "Driver", "100" };
+	vector<string> data = {"Surname_2 First_name_2 Second_name_2", "Driver", "150"};
 	Employee* employee = EF.create(data);
 	employee->calculate_payment();
 	int emp_payment = employee->get_payment();
-	EXPECT_EQ(emp_payment, 16000);
+	EXPECT_EQ(emp_payment, 24000);
 }
